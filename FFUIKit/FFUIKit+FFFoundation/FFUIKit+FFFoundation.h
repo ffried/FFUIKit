75786da0d676f9a689b54eed68265b444b9ff3a0
Florian Friedrich <ffried@me.com>
2015-01-17 19:11:32 +0100

    Added FFUIKit_FFFoundation

---
+++ FFUIKit/FFUIKit+FFFoundation/FFUIKit+FFFoundation.h
@@ -0,0 +1,19 @@
+//
+//  FFUIKit+FFFoundation.h
+//  FFUIKit+FFFoundation
+//
+//  Created by Florian Friedrich on 17.1.15.
+//  Copyright (c) 2015 Florian Friedrich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FFUIKit+FFFoundation.
+FOUNDATION_EXPORT double FFUIKit_FFFoundationVersionNumber;
+
+//! Project version string for FFUIKit+FFFoundation.
+FOUNDATION_EXPORT const unsigned char FFUIKit_FFFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFUIKit_FFFoundation/PublicHeader.h>
+
+
