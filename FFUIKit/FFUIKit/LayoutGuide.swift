f91cf306417ab83a52472aa80548bd6810793f78
Florian Friedrich <ffried@me.com>
2015-02-11 20:14:31 +0100

    Added LayoutGuide

---
+++ FFUIKit/FFUIKit/LayoutGuide.swift
@@ -0,0 +1,20 @@
+//
+//  LayoutGuide.swift
+//  FFUIKit
+//
+//  Created by Florian Friedrich on 11.2.15.
+//  Copyright (c) 2015 Florian Friedrich. All rights reserved.
+//
+
+import FFUIkit
+
+public class LayoutGuide: NSObject, UILayoutSupport {
+    public private(set) var length: CGFloat
+    
+    public required init(length: CGFloat = 0.0) {
+        self.length = length
+        super.init()
+    }
+    
+    override convenience init() { self.init(length: 0.0) }
+}
