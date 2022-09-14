114bfc323401f71fbc3d0cd5947d35b67b71cd27
Florian Friedrich <ffried@me.com>
2022-09-14 11:33:00 +0200

    Add Swift 5.7 support

--- Package@swift-5.6.swift
+++ Package@swift-5.6.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,19 +7,18 @@ import Foundation
 let package = Package(
     name: "FFUIKit",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v11),
+        .macOS(.v10_12),
+        .iOS(.v10),
         .watchOS(.v4),
-        .tvOS(.v11),
+        .tvOS(.v10),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "FFUIKit",
-                 targets: ["FFUIKit"]),
+        .library(name: "FFUIKit", targets: ["FFUIKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/ffried/FFFoundation.git", from: "9.4.0"),
+        .package(url: "https://github.com/ffried/FFFoundation.git", from: "9.1.0"),
         .package(url: "https://github.com/sersoft-gmbh/color-components.git", from: "1.2.0"),
     ],
     targets: [
